7979d4ee6ebfd13782c083e19e11ffb4742f843d
Dennis Glennon <djglenno@csc235x20.ad.calpoly.edu>
2015-05-26 16:45:47 -0700

    pathing display

--- MobileAnimatedActor.java
+++ MobileAnimatedActor.java
@@ -1,40 +1,69 @@
 import processing.core.PImage;
 import java.util.List;
+import java.util.ArrayList;
 import static java.lang.Math.abs;
 
 public abstract class MobileAnimatedActor
-   extends AnimatedActor
+    extends AnimatedActor
 {
-   public MobileAnimatedActor(String name, Point position, int rate,
-      int animation_rate, List<PImage> imgs)
-   {
-      super(name, position, rate, animation_rate, imgs);
-   }
+    private List<Point> path;
+    private List<Point> checked;
 
-   protected Point nextPosition(WorldModel world, Point dest_pt)
-   {
-      int horiz = Integer.signum(dest_pt.x - getPosition().x);
-      Point new_pt = new Point(getPosition().x + horiz, getPosition().y);
+    public MobileAnimatedActor(String name, Point position, int rate,
+        int animation_rate, List<PImage> imgs)
+    {
+        super(name, position, rate, animation_rate, imgs);
+        this.path = new ArrayList<>();
+        this.checked = new ArrayList<>();
+        this.checked.add(new Point(1, 1));
+        this.path.add(new Point(1, 1));
+        this.path.add(new Point(1, 2));
+    }
 
-      if (horiz == 0 || !canPassThrough(world, new_pt))
-      {
-         int vert = Integer.signum(dest_pt.y - getPosition().y);
-         new_pt = new Point(getPosition().x, getPosition().y + vert);
+    public List<Point> get_path()
+	{
+        return this.path;
+	}
 
-         if (vert == 0 || !canPassThrough(world, new_pt))
-         {
-            new_pt = getPosition();
-         }
-      }
+    public List<Point> get_checked()
+	{
+        return this.checked;
+	}
 
-      return new_pt;
-   }
+    public void add_to_path(Point p)
+	{
+        this.path.add(p);
+	}
 
-   protected static boolean adjacent(Point p1, Point p2)
-   {
-      return (p1.x == p2.x && abs(p1.y - p2.y) == 1) ||
-         (p1.y == p2.y && abs(p1.x - p2.x) == 1);
-   }
+    public void add_to_checked(Point p)
+	{
+        this.checked.add(p);
+	}
 
-   protected abstract boolean canPassThrough(WorldModel world, Point new_pt);
+    protected Point nextPosition(WorldModel world, Point dest_pt)
+    {
+        int horiz = Integer.signum(dest_pt.x - getPosition().x);
+        Point new_pt = new Point(getPosition().x + horiz, getPosition().y);
+
+        if (horiz == 0 || !canPassThrough(world, new_pt))
+        {
+            int vert = Integer.signum(dest_pt.y - getPosition().y);
+            new_pt = new Point(getPosition().x, getPosition().y + vert);
+
+            if (vert == 0 || !canPassThrough(world, new_pt))
+            {
+                new_pt = getPosition();
+            }
+        }
+
+        return new_pt;
+    }
+
+    protected static boolean adjacent(Point p1, Point p2)
+    {
+        return (p1.x == p2.x && abs(p1.y - p2.y) == 1) ||
+            (p1.y == p2.y && abs(p1.x - p2.x) == 1);
+    }
+
+    protected abstract boolean canPassThrough(WorldModel world, Point new_pt);
 }
